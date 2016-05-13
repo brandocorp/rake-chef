d05e5a4d3a94a8c38ac2b5e9059489e14ba494b6
Brandon Raabe <brandocorp@gmail.com>
2016-05-13 16:16:31 -0700

    fix dsl

--- lib/rake/chef.rb
+++ lib/rake/chef.rb
@@ -1,9 +1,3 @@
 require "rake/chef/version"
 require 'rake/chef/dsl'
-require 'rake'
 
-module Rake
-  module Chef
-    ::Rake::DSL.send(:include, Rake::Chef::DSL)
-  end
-end
--- lib/rake/chef/dsl.rb
+++ lib/rake/chef/dsl.rb
@@ -1,25 +1,25 @@
+require 'chef'
+
 module Rake
-  module Chef
-    module DSL
+  module DSL
 
-      private
+    private
 
-      def recipe(id, &block)
-        namespace :chef do
-          namespace :recipe do
-            desc "Run chef rake task #{id}"
-            task id do
-              Chef::Config[:solo] = true
-              @chef_client = Chef::Client.new
-              @chef_client.run_ohai
-              @chef_client.load_node
-              @chef_client.build_node
-              run_context = Chef::RunContext.new(@chef_client.node, {}, @chef_client.events)
-              recipe = Chef::Recipe.new("(rake-chef cookbook)", "(rake-chef recipe)", run_context)
-              recipe.instance_eval(&block)
-              runner = Chef::Runner.new(run_context)
-              runner.converge
-            end
+    def recipe(id, &block)
+      namespace :chef do
+        namespace :recipe do
+          desc "Run chef rake task #{id}"
+          task id do
+            ::Chef::Config[:solo] = true
+            @chef_client = ::Chef::Client.new
+            @chef_client.run_ohai
+            @chef_client.load_node
+            @chef_client.build_node
+            run_context = ::Chef::RunContext.new(@chef_client.node, {}, @chef_client.events)
+            recipe = ::Chef::Recipe.new("(rake-chef cookbook)", "(rake-chef recipe)", run_context)
+            recipe.instance_eval(&block)
+            runner = ::Chef::Runner.new(run_context)
+            runner.converge
           end
         end
       end
